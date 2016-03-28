a2880f983154959b7fe39e88a1e6632193fcfd4c
gdi2290 <github@gdi2290.com>
2016-03-27 22:41:27 -0700

    refactor(index): move into hmr.ts

---
+++ src/hmr.ts
@@ -0,0 +1,100 @@
+import {WebpackStore} from './webpack-store';
+
+export interface HotModuleReplacementOptions {
+  LOCALSTORAGE_KEY?: string;
+  localStorage?: boolean;
+  storeToken?: any;
+  globalDispose?: string;
+
+  getState?: Function;
+  data?: any;
+}
+
+export function hotModuleReplacement(bootloader: Function, module: any, options: HotModuleReplacementOptions = {}) {
+  const LOCALSTORAGE_KEY = options.LOCALSTORAGE_KEY || '@@WEBPACK_INITIAL_DATA';
+  const LOCAL            = options.localStorage     || false;
+  const TOKEN            = options.storeToken       || WebpackStore;
+  const DISPOSE          = options.globalDispose    || 'WEBPACK_HMR_beforeunload';
+  const GET_STATE        = options.getState         || getState;
+  let DATA               = options.data             || module.hot.data.state;
+  let COMPONENT_REF = null;
+  let disposed = false;
+
+  function getState(appState) {
+    const json = appState.toJSON();
+
+    if (LOCAL) {
+      console.time('localStorage');
+      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(appState));
+      console.timeEnd('localStorage');
+    }
+    return json;
+  }
+
+  console.log('DATA', DATA);
+  if (!DATA && LOCAL) {
+    try {
+      console.time('start localStorage');
+      DATA = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || DATA;
+      console.timeEnd('start localStorage');
+    } catch (e) {
+      console.log('JSON.parse Error', e);
+    }
+  }
+  console.time('bootstrap');
+  if (document.readyState === 'complete') {
+    bootloader(DATA)
+      .then((cmpRef: any) => COMPONENT_REF = cmpRef)
+      .then((cmpRef => (console.timeEnd('bootstrap'), cmpRef)));
+  } else {
+    document.addEventListener('DOMContentLoaded', () => {
+      bootloader(DATA)
+        .then((cmpRef: any) => COMPONENT_REF = cmpRef)
+        .then((cmpRef => (console.timeEnd('bootstrap'), cmpRef)));
+    });
+  }
+
+
+
+  function beforeunload(event) {
+    const appState = COMPONENT_REF.injector.get(TOKEN);
+    return GET_STATE(appState);
+  }
+  (<any>window)[DISPOSE] = () => {
+    disposed = true;
+    window.removeEventListener('beforeunload', beforeunload);
+    if (LOCAL) {
+      localStorage.removeItem(LOCALSTORAGE_KEY);
+    }
+  };
+
+  module.hot.accept();
+
+  window.addEventListener('beforeunload', beforeunload);
+
+  module.hot.dispose((data: any) => {
+    console.time('dispose');
+    const componentNode = COMPONENT_REF.location.nativeElement;
+    const newNode = document.createElement(componentNode.tagName);
+    // display none
+    const currentDisplay = newNode.style.display;
+    newNode.style.display = 'none';
+    const parentNode = componentNode.parentNode;
+    parentNode.insertBefore(newNode, componentNode);
+
+    const appState = COMPONENT_REF.injector.get(TOKEN);
+    const json = GET_STATE(appState);
+
+    data.state = json;
+
+    COMPONENT_REF.dispose();
+
+    newNode.style.display = currentDisplay;
+
+    if (!disposed) {
+      window.removeEventListener('beforeunload', beforeunload);
+    }
+    disposed = true;
+    console.timeEnd('dispose');
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,104 +1,14 @@
-import {WebpackState} from './webpack-state';
-export * from './webpack-state';
+import {Provider} from 'angular2/core';
+import {WEBPACK_HMR, WebpackStore} from './webpack-store';
 
-export interface HotModuleReplacementOptions {
-  LOCALSTORAGE_KEY?: string;
-  localStorage?: boolean;
-  storeToken?: any;
-  globalDispose?: string;
-  saveState?: Function;
-  assignState?: Function;
-  data?: any;
-}
-
-export function hotModuleReplacement(bootloader: Function, module: any, options: HotModuleReplacementOptions = {}) {
-  const LOCALSTORAGE_KEY = options.LOCALSTORAGE_KEY || '@@WEBPACK_INITIAL_DATA';
-  const LOCAL = options.localStorage || false;
-  const TOKEN = options.storeToken || WebpackState;
-  const DISPOSE = options.globalDispose || 'WEBPACK_HMR_beforeunload';
-  const SAVE_STATE = options.saveState || saveState;
-  const ASSIGN = options.assignState || (<any>Object).assign;
-  let disposed = false;
-  let DATA = options.data || module.hot.data;
+export * from './hmr';
+export * from './webpack-decorator';
+export * from './webpack-store';
 
 
-  let COMPONENT_REF = null;
-
-  function saveState(appState) {
-    const json = appState.toJSON();
-
-    if (LOCAL) {
-      console.time('localStorage');
-      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(appState));
-      console.timeEnd('localStorage');
-    }
-    return json;
-  }
-
-  console.log('DATA', DATA);
-  if (!DATA && LOCAL) {
-    try {
-      console.time('start localStorage');
-      DATA = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || DATA;
-      console.timeEnd('start localStorage');
-    } catch (e) {
-      console.log('JSON.parse Error', e);
-    }
-  }
-  console.time('bootstrap');
-  if (document.readyState === 'complete') {
-    bootloader(DATA)
-      .then((cmpRef: any) => COMPONENT_REF = cmpRef)
-      .then((cmpRef => (console.timeEnd('bootstrap'), cmpRef)));
-  } else {
-    document.addEventListener('DOMContentLoaded', () => {
-      bootloader(DATA)
-        .then((cmpRef: any) => COMPONENT_REF = cmpRef)
-        .then((cmpRef => (console.timeEnd('bootstrap'), cmpRef)));
-    });
-  }
-
-
-
-  function beforeunload(event) {
-    const appState = COMPONENT_REF.injector.get(TOKEN);
-    return SAVE_STATE(appState);
-  }
-  (<any>window)[DISPOSE] = () => {
-    disposed = true;
-    window.removeEventListener('beforeunload', beforeunload);
-    if (LOCAL) {
-      localStorage.removeItem(LOCALSTORAGE_KEY);
-    }
-  };
-
-  module.hot.accept();
-
-  window.addEventListener('beforeunload', beforeunload);
-
-  module.hot.dispose(data => {
-    console.time('dispose');
-    const componentNode = COMPONENT_REF.location.nativeElement;
-    const newNode = document.createElement(componentNode.tagName);
-    // display none
-    const currentDisplay = newNode.style.display;
-    newNode.style.display = 'none';
-    const parentNode = componentNode.parentNode;
-    parentNode.insertBefore(newNode, componentNode);
-
-    const appState = COMPONENT_REF.injector.get(TOKEN);
-    const json = SAVE_STATE(appState);
-
-    ASSIGN(data, json);
-
-    COMPONENT_REF.dispose();
-
-    newNode.style.display = currentDisplay;
-
-    if (!disposed) {
-      window.removeEventListener('beforeunload', beforeunload);
-    }
-    disposed = true;
-    console.timeEnd('dispose');
-  });
+export function provideInitialState(initialState = {}): Array<Provider> {
+  return [
+    new Provider(WEBPACK_HMR, {useValue: initialState }),
+    new Provider(WebpackStore, {useClass: WebpackStore})
+  ];
 }
