d46fca0b99eaf62271b4bd5fee2d6b44bb436dc5
OscarGalindo <oscar.galindo@apiumtech.com>
2016-10-05 17:03:05 +0200

    noImplicitReturns and noImplicitAny to true and fixed in source

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,5 @@
 // Hot Module Replacement
-export function bootloader(main) {
+export function bootloader(main:any) {
   if (document.readyState === 'complete') {
     main()
   } else {
@@ -9,8 +9,8 @@ export function bootloader(main) {
 
 
 // create new elements
-export function createNewHosts(cmps) {
-  const components = cmps.map(componentNode => {
+export function createNewHosts(cmps:any) {
+  const components = cmps.map((componentNode:any) => {
     const newNode = document.createElement(componentNode.tagName);
     // display none
     const currentDisplay = newNode.style.display;
@@ -20,7 +20,7 @@ export function createNewHosts(cmps) {
     return { currentDisplay, newNode };
   });
   return () => {
-    components.forEach(cmp => {
+    components.forEach((cmp:any) => {
       cmp.newNode.style.display = cmp.currentDisplay;
       cmp.newNode = null;
       cmp.currentDisplay = null;
@@ -32,21 +32,21 @@ export function createNewHosts(cmps) {
 // remove old styles
 export function removeNgStyles() {
   Array.prototype.slice.call(document.head.querySelectorAll('style'), 0)
-    .filter((style) => style.innerText.indexOf('_ng') !== -1)
-    .map(el => el.remove());
+    .filter((style:any) => style.innerText.indexOf('_ng') !== -1)
+    .map((el:any) => el.remove());
 }
 
 // get input values
 export function getInputValues() {
   const inputs = document.querySelectorAll('input');
-  return Array.prototype.slice.call(inputs).map(input => input.value);
+  return Array.prototype.slice.call(inputs).map((input:any) => input.value);
 }
 
 // set input values
-export function setInputValues($inputs) {
+export function setInputValues($inputs:any) {
   const inputs = document.querySelectorAll('input');
   if ($inputs && inputs.length === $inputs.length) {
-    $inputs.forEach((value, i) => {
+    $inputs.forEach((value:any, i:number) => {
       const el: any = inputs[i];
       el.value = value;
       el.dispatchEvent(new CustomEvent('input', {detail: el.value}));
